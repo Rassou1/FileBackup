06e15d8c4e9eb65ed0297fb7e0c2c8b1c81aff90
Ibrahim <ibrra187@gmail.com>
2024-06-06 01:05:26 +0200

    now maybe

---
+++ Backup/FileBackup.cs
@@ -0,0 +1,105 @@
+﻿using A5_FileBackupCS.FileManagerFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5_FileBackupCS.Backup
+{
+    internal class FileBackup
+    {
+        List<FileData>fileDataList = new List<FileData>();
+        long timeTaken = 0;
+        public long TimeTaken => timeTaken;
+        object lockObject = new object();
+
+        public List<FileData> StartBackup(string[] filesToBackUp)
+        {
+            int taskCount = filesToBackUp.Length;
+            int remainingTasks = taskCount;
+            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+
+            foreach(string s in filesToBackUp)
+            {
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    try
+                    {
+                        FileData result = BackupFile(s);
+                        lock (fileDataList)
+                        {
+                            fileDataList.Add(result);
+                        }
+                    }
+                    finally
+                    {
+                        lock (lockObject)
+                        {
+                            remainingTasks--;
+                        }
+                    }
+                });
+                
+            }
+            while (remainingTasks > 0)
+            {
+                Thread.Sleep(100);
+            }
+
+            stopWatch.Stop();
+            timeTaken = stopWatch.ElapsedMilliseconds;
+
+            return new List<FileData>(fileDataList);
+
+        }
+
+        FileData BackupFile(string sourcePath)
+        {
+            FileData fileData = new FileData(sourcePath);
+            if(!File.Exists(sourcePath))
+            {
+             
+                    string errorMsg = $"The file at {sourcePath} does not exist.";
+                fileData.ErrorMsg = errorMsg;
+                return fileData;
+            }
+
+            string backupPath = Path.Combine(fileData.PathOnly, "BACKUP", fileData.FileName);
+            bool clearForBackup = CopyFiles(sourcePath, backupPath);
+            if(clearForBackup)
+            {
+                fileData.BkpFullPath = backupPath;
+            }
+            else
+            {
+                fileData.ErrorMsg = "Failed to copy file.";
+            }
+            return fileData;
+        }
+
+
+        bool CopyFiles(string sourceFilePath, string targetFilePath)
+        {
+            try 
+            {
+                string directoryPath = Path.GetDirectoryName(targetFilePath);
+                if(!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                File.Copy(sourceFilePath, targetFilePath, true);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+    }
+     
+}
---
+++ Backup/FileBackupAsync.cs
@@ -0,0 +1,71 @@
+﻿using A5_FileBackupCS.FileManagerFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5_FileBackupCS.Backup
+{
+    internal class FileBackupAsync
+    {
+        List<FileData> fileDataList = new List<FileData>();
+        long timeTaken = 0;
+        public long TimeTaken => timeTaken;
+
+        public async Task<List<FileData>> StartBackupAsync(string[] filesToBackUp)
+        {
+            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+            List<Task<FileData>> tasks = new List<Task<FileData>>();
+
+            foreach(string filePath in filesToBackUp)
+            {
+                tasks.Add(BackupFileAsync(filePath));
+            }
+
+            var results = await Task.WhenAll(tasks);
+            lock (fileDataList)
+            {
+                fileDataList.AddRange(results);
+            }
+            stopWatch.Stop();
+            timeTaken = stopWatch.ElapsedMilliseconds;
+            return fileDataList;
+        }
+
+        public async Task<FileData> BackupFileAsync(string sourcePath)
+        {
+            FileData fileData = new FileData(sourcePath);
+            if(!File.Exists(sourcePath))
+            {
+                string errorMsg = "Following source file does not exist: " +  sourcePath;
+                fileData.ErrorMsg= errorMsg;
+                return fileData;
+            }
+
+            string backupPath = Path.Combine(fileData.PathOnly, "BACKUP ASYNC", fileData.FileName);
+            try
+            {
+                await CopyFilesAsync(sourcePath, backupPath);
+                fileData.BkpFullPath = backupPath;  
+            }
+            catch(Exception ex)
+            {
+                fileData.ErrorMsg = "Failed to copy file. For more details: " + ex.Message;
+            }
+            return fileData;
+
+        }
+
+        public async Task CopyFilesAsync(string sourceFilePath, string targetFilePath)
+        {
+            string directoryPath = Path.GetDirectoryName(targetFilePath);
+            if(!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            
+            File.Copy(sourceFilePath, targetFilePath, true);
+        }
+    }
+}
